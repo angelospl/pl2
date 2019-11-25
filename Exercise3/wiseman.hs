44c165c7876d5b16398797af36c4259bb1d8c96d
angelospl <angelospl48@gmail.com>
2019-11-25 21:13:45 +0200

    Implement recursive function for dp.Memo not working

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -1,5 +1,6 @@
 module Main where
 
+
 data Game = Win | Loss
 
 instance Eq Game where
@@ -24,6 +25,42 @@ points (game:tl) prevs acc= let newpoints=point_eval game prevs in
                             if newpoints == -1 then -1
                             else points tl newpoints (acc+newpoints)
 
+first::(a,b,c)->a
+first(x,_,_)=x
+
+second::(a,b,c)->b
+second(_,x,_)=x
+
+third::(a,b,c)->c
+third(_,_,x)=x
+
+
+memoize::(Int->Int->a)->[[a]]
+memoize f=map (\x-> map (f x) [0..]) [0..]
+
+dp::Int->Int->(Int,Int,Int)
+dp 0 0=(0,1,0)
+dp 0 1=(0,1,0)
+dp 0 2=(0,0,1)
+dp i 0 =(0,0,0)
+dp 0 j =(0,0,0)
+dp 1 1 =(1,0,0)
+dp 1 2 =(1,1,0)
+dp i j=((wins ((floor.logBase 2 .fromIntegral)(i+1)) 0 i j),first(dp i (j-1)),second (dp i (j-1)))
+  where wins 0 acc _ _=acc
+        wins winstreak acc i j
+          | i<0 || j<0 = acc
+          | otherwise = wins (winstreak-1) (acc+second(dp (i-(2^winstreak-1)) (j-winstreak))) i j
+
+
+dpstore::[[(Int,Int,Int)]]
+dpstore=memoize dp
+
+fastdp::Int->Int->(Int,Int,Int)
+fastdp x y=dpstore !! x !! y
+
+
 
 main :: IO ()
-main = putStrLn "Hello World"
+main =
+  putStrLn $ show (dp 37 37)
