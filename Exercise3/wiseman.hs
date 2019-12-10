adbaa562a69fbeb4c6d81af020a8e70cc9cefbfa
angelospl <angelospl48@gmail.com>
2019-12-11 00:21:37 +0200

    Array implementation for impure.

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -1,77 +1,28 @@
 module Main where
-
-
-data Game = Win | Loss
-
-instance Eq Game where
-  (==) Win Win = True
-  (==) Loss Loss = True
-  (==) _ _ = False
-
---takes a game an integer that shows the previous score and returns the score
-point_eval::Game -> Integer -> Integer
-point_eval game prev= if (game == Win)
-                      then if prev > 0
-                           then 2*prev
-                           else 1
-                      else if prev > 0
-                           then 0
-                           else -1
-
---a function that takes a list and returns the points
-points::[Game]-> Integer -> Integer-> Integer
-points [] _ acc=acc
-points (game:tl) prevs acc= let newpoints=point_eval game prevs in
-                            if newpoints == -1 then -1
-                            else points tl newpoints (acc+newpoints)
-
-first::(a,b,c)->a
-first(x,_,_)=x
-
-second::(a,b,c)->b
-second(_,x,_)=x
-
-third::(a,b,c)->c
-third(_,_,x)=x
-
-
-dp::Int->Int->(Int,Int,Int)
-dp 0 0=(0,1,0)
-dp 0 1=(0,1,0)
-dp 0 2=(0,0,1)
-dp i 0 =(0,0,0)
-dp 0 j =(0,0,0)
-dp 1 1 =(1,0,0)
-dp 1 2 =(1,1,0)
-dp i j=((wins ((floor.logBase 2 .fromIntegral)(i+1)) 0 i j),first(dp i (j-1)),second (dp i (j-1)))
-  where wins 0 acc _ _=acc
-        wins winstreak acc i j
-          | i<0 || j<0 = acc
-          | otherwise = wins (winstreak-1) (acc+second(dp (i-(2^winstreak-1)) (j-winstreak))) i j
-
-
-dp_solution n=sum [third (dp n y)| y <-[1..(2^n)+2]]
+import Data.Array
+import Data.Char
 
 wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
-helper::Int->Integer
+helper::Integer->Integer
 helper 0=1
 helper 1=1
 helper n=
   let streaklist= takeWhile (\x -> x <= n) wins in
-  (sum [(fast_helper (n-streakpoints)) `mod` 2019 | streakpoints <-wins, streakpoints<=n]) `mod` 2019
+  (sum [(fast_helper !(n-streakpoints)) `mod` 1000000007 | streakpoints <-wins, streakpoints<=n]) `mod` 1000000007
 
-fast_helper::Int->Integer
-fast_helper= (map helper [0..] !!)
+fast_helper= listArray (0,n-1) $ map helper [0..n]
+  where n=1000000
 
-solution::Int->Integer
+solution::Integer->Integer
 solution 0=1
 solution n=2*(helper n)
 
-solution_diff::Int->Int->Integer
+solution_diff::Integer->Integer->Integer
 solution_diff a b=
-  sum [(solution x) `mod` 2019 | x <- [a..b]] `mod` 2019
+  sum [(solution x) `mod` 1000000007 | x <- [a..b]] `mod` 1000000007
+
 
 main :: IO ()
-main =
-  putStrLn $ show (dp 37 37)
+main = do
+  print $ solution_diff 500000 999999
