974b9088989c04f3b05be3079a0b2e25fb20767d
angelospl <angelospl48@gmail.com>
2019-12-21 20:25:43 +0200

    Read Input.Made it slower

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -1,28 +1,36 @@
 module Main where
 import Data.Array
 import Data.Char
+import Control.Monad.Cont
+
 
 wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
-helper::Integer->Integer
-helper 0=1
-helper 1=1
-helper n=
+helper::Integer->Integer->Integer
+helper _ 0=1
+helper _ 1=1
+helper m n=
   let streaklist= takeWhile (\x -> x <= n) wins in
-  (sum [(fast_helper !(n-streakpoints)) `mod` 1000000007 | streakpoints <-wins, streakpoints<=n]) `mod` 1000000007
+  (sum [((fast_helper $! m) !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
 
-fast_helper= listArray (0,n-1) $ map helper [0..n]
+fast_helper::Integer->Array Integer Integer
+fast_helper m= listArray (0,n-1) $ map (helper m) [0..n]
   where n=1000000
 
-solution::Integer->Integer
-solution 0=1
-solution n=2*(helper n)
+solution::Integer->Integer->Integer
+solution 0 _=1
+solution n m=2*(helper m n)
 
-solution_diff::Integer->Integer->Integer
-solution_diff a b=
-  sum [(solution x) `mod` 1000000007 | x <- [a..b]] `mod` 1000000007
+solution_diff::(Integer,Integer,Integer)->Integer
+solution_diff (a,b,m) =
+  (sum [(solution x m) `mod`  m | x <- [a..b]]) `mod` m
 
+readInts :: IO [Integer]
+readInts = fmap (map read.words) getLine
 
 main :: IO ()
 main = do
-  print $ solution_diff 500000 999999
+  [n,m] <- readInts
+  forM_ [1..n] $ \i -> do
+    [a,b] <- readInts
+    print $ solution_diff $!(a,b,m)
