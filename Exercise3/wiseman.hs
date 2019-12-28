632c72889bebe596f83358013f9de7bd5b06ba1d
angelospl <angelospl48@gmail.com>
2019-12-28 21:10:40 +0200

    All functions inside solution_diff

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -6,20 +6,20 @@ import Control.Monad.Cont
 
 wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
-solution::Integer->Integer->Integer
-solution 0 _=1
-solution n m=2*(helper n)
-  where helper 0=1
-        helper 1=1
-        helper n=
-          let streaklist= takeWhile (\x -> x <= n) wins in
-          (sum [(fast_helper !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
-          where fast_helper= listArray (0,n-1) $ map helper [0..n]
+solution_diff::([Integer],Integer)->Integer
+solution_diff (lista,m) =
+  (sum [(solution $! x) `mod`  m | x <-lista ]) `mod` m
+  where solution 0=1
+        solution n=2*(fast_helper ! n)
+          where fast_helper=listArray (0,n-1) $ map helper [0..n]
                   where n=1000000
+                        helper 0=1
+                        helper 1=1
+                        helper n=
+                          let streaklist= takeWhile (\x -> x <= n) wins in
+                          (sum [(fast_helper !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
 
-solution_diff::(Integer,Integer,Integer)->Integer
-solution_diff (a,b,m) =
-  (sum [(solution x $! m) `mod`  m | x <- [a..b]]) `mod` m
+
 
 readInts :: IO [Integer]
 readInts = fmap (map read.words) getLine
@@ -28,5 +28,5 @@ main :: IO ()
 main = do
   [n,m] <- readInts
   forM_ [1..n] $ \i -> do
-    [a,b] <- readInts 
-    print $ solution_diff $!(a,b,m)
+    [a,b] <- readInts
+    print $ solution_diff $!([a..b],m)
