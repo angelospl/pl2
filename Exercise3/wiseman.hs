2331b8549c648d803dce7f7cb933d0dd9d5d3a2b
angelospl <angelospl48@gmail.com>
2019-12-30 18:11:18 +0200

    Minor change

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -8,14 +8,20 @@ wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
 solution::Integer->Integer->Integer
 solution 0 _=1
-solution n m=2*(helper n)
-  where helper 0=1
-        helper 1=1
-        helper n=
-          let streaklist= takeWhile (\x -> x <= n) wins in
-          (sum [(fast_helper !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
-          where fast_helper= listArray (0,n-1) $ map helper [0..n]
-                  where n=1000000
+solution n m=2*(helper m n)
+
+helper::Integer->Integer->Integer
+helper _ 0=1
+helper _ 1=1
+helper m n=
+  let streaklist= takeWhile (\x -> x <= n) wins in
+  (sum [(score_array !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
+    where score_array = fast_helper m
+
+fast_helper::Integer->Array Integer Integer
+fast_helper m= listArray (0,n-1) $ map (helper m) [0..n]
+        where n=1000000
+
 
 solution_diff::(Integer,Integer,Integer)->Integer
 solution_diff (a,b,m) =
@@ -28,5 +34,5 @@ main :: IO ()
 main = do
   [n,m] <- readInts
   forM_ [1..n] $ \i -> do
-    [a,b] <- readInts 
+    [a,b] <- readInts
     print $ solution_diff $!(a,b,m)
