2a18d7ecff2efc91aafc48ac66e3ec897ada3134
angelospl <angelospl48@gmail.com>
2019-12-30 19:09:54 +0200

    Not a change

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -8,16 +8,18 @@ wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
 solution_diff::([Integer],Integer)->Integer
 solution_diff (lista,m) =
-  (sum [(solution $! x) `mod`  m | x <-lista ]) `mod` m
-  where solution 0=1
-        solution n=2*(fast_helper ! n)
-          where fast_helper=listArray (0,n-1) $ map helper [0..n]
-                  where n=1000000
-                        helper 0=1
-                        helper 1=1
-                        helper n=
-                          let streaklist= takeWhile (\x -> x <= n) wins in
-                          (sum [(fast_helper !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
+  (sum [(solution x m $! fast_helper) `mod`  m | x <-lista ]) `mod` m
+  where fast_helper={-# SCC fast #-}listArray (0,n-1) $ map helper [0..n]
+            where n=10000
+                  helper 0=1
+                  helper 1=1
+                  helper n={-# SCC helper #-}
+                    let streaklist= takeWhile (\x -> x <= n) wins in
+                    (sum [({-# SCC index #-}fast_helper !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
+
+
+solution 0 _ _=1
+solution n m fast_helper=2*(fast_helper ! n)
 
 
 
