c917bac82800198a045d824c216cad4358c337c9
angelospl <angelospl48@gmail.com>
2020-01-03 19:55:17 +0200

    Faster edition so far not good enough

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -2,30 +2,33 @@ module Main where
 import Data.Array
 import Data.Char
 import Control.Monad.Cont
-
+import Control.Monad.Fix
 
 wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
-solution::Integer->Integer->Integer
-solution 0 _=1
-solution n m=2*(helper m n)
+solution::Integer->Integer
+solution 0=1
+solution n=2*(superfast_helper $! n)
 
-helper::Integer->Integer->Integer
+helper::(Integer->Integer)->Integer->Integer
 helper _ 0=1
 helper _ 1=1
-helper m n=
+helper f n=
   let streaklist= takeWhile (\x -> x <= n) wins in
-  (sum [(score_array !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
-    where score_array = fast_helper m
+  (sum [(f (n-streakpoints)) | streakpoints <-streaklist])
 
-fast_helper::Integer->Array Integer Integer
-fast_helper m= listArray (0,n-1) $ map (helper m) [0..n]
-        where n=1000000
 
+fast_helper::(Integer->Integer)->(Integer->Integer)
+fast_helper f=(\x -> list ! x)
+                where list= listArray (0,n-1) $ map f [0..n]
+                            where n=1000000
+
+superfast_helper::Integer->Integer
+superfast_helper=fix (fast_helper . helper)
 
 solution_diff::(Integer,Integer,Integer)->Integer
 solution_diff (a,b,m) =
-  (sum [(solution x $! m) `mod`  m | x <- [a..b]]) `mod` m
+  (sum [(solution x) `mod`  m | x <- [a..b]]) `mod` m
 
 readInts :: IO [Integer]
 readInts = fmap (map read.words) getLine
