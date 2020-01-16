7636f105a784c3903bc29e051780524c22d83fee
angelospl <angelospl48@gmail.com>
2020-01-16 21:14:33 +0200

    Add create_array. Seems good

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -6,20 +6,21 @@ import Control.Monad.Cont
 
 wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
-solution_diff::([Integer],Integer)->Integer
-solution_diff (lista,m) =
-  (sum [(solution x m $! fast_helper) `mod`  m | x <-lista ]) `mod` m
-    where fast_helper={-# SCC fast #-}listArray (0,n-1) $ map helper $! [0..n]
-            where n=1000000
-                  helper 0=1
-                  helper 1=1
-                  helper n={-# SCC helper #-}
-                    let streaklist= takeWhile (\x -> x <= n) wins in
-                    (sum [({-# SCC index #-}fast_helper !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
+solution_diff::([Integer],Integer)->Array Integer Integer->Integer
+solution_diff (lista,m) array=
+   (sum [(solution x array) `mod`  m | x <-lista ]) `mod` m
+
+create_array::Integer->Array Integer Integer
+create_array m= ret
+  where ret= array (0,999999) ((0,1):(1,2):[(i,helper i)| i<-[2..999999]])
+        helper n=
+            let streaklist= takeWhile (\x -> x <= n) wins in
+            (sum [(ret !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
 
 
+solution::Integer->Array Integer Integer
 solution 0 _ _=1
-solution n m fast_helper=2*(fast_helper ! n)
+solution n array=array!n
 
 
 
@@ -29,6 +30,3 @@ readInts = fmap (map read.words) getLine
 main :: IO ()
 main = do
   [n,m] <- readInts
-  forM_ [1..n] $ \i -> do
-    [a,b] <- readInts
-    print $ solution_diff $!([a..b],m)
