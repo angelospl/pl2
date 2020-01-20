e95d66cfa7661fc0efad0b1ef56886c664698f12
angelospl <angelospl48@gmail.com>
2020-01-20 21:57:30 +0200

    Impure fastest solution. Needs a bug fix.

--- Exercise3/wiseman.hs
+++ Exercise3/wiseman.hs
@@ -1,26 +1,35 @@
 module Main where
-import Data.Array
+import Data.Array.IO
+import Data.Array.MArray
+import Data.Array.IArray
 import Data.Char
+import Data.Int
 import Control.Monad.Cont
 import Control.Monad.Fix
 
 wins =[2^i-1 | i <- [1..19]] --all points from wins in an infinite list
 
-solution_diff::(Integer,Integer,Integer)->Array Integer Integer->Integer
+solution_diff::(Int64,Int64,Int64)->Array Int64 Int64->Int64
 solution_diff (a,b,m) array=
    (sum [(solution x) `mod`  m | x <- [a..b]]) `mod` m
-    where solution 0=1
-          solution x=2*(array!x)
+      where solution 0=1
+            solution x= 2*(array!x)
 
-create_array::Integer->Integer->Array Integer Integer
-create_array m maxN= ret
-  where ret= array (0,maxN) ((0,1):(1,1):[(i,helper i)| i<-[2..maxN]])
-        helper n=
-            let streaklist= takeWhile (\x -> x <= n) wins in
-            (sum [(ret !(n-streakpoints)) `mod` m | streakpoints <-streaklist]) `mod` m
+create_array::Int64->Int64->IO (Array Int64 Int64)
+create_array m maxN=do
+  arr <- newArray (0,1000000) 0::IO (IOUArray Int64 Int64)
+  writeArray arr 0 1
+  writeArray arr 1 1
+  forM_ [0..1000000] $ (\i -> do
+     val <- readArray arr i
+     let j_list = takeWhile (\x -> x + i <= maxN ) wins
+     forM_ j_list $ (\j -> do
+       prev <-readArray arr j
+       writeArray arr (j+i) ((val `mod` m) + (prev `mod` m) `mod` m)))
+  ret <- freeze arr
+  return ret
 
-
-read_input::Integer->[Integer]->IO [Integer]
+read_input::Int64->[Int64]->IO [Int64]
 read_input n lista =
   if n==0 then do return lista
   else do
@@ -28,17 +37,18 @@ read_input n lista =
     read_input (n-1) (lista++x)
 
 
-print_results::[Integer]->Integer->Array Integer Integer-> IO ()
+print_results::[Int64]->Int64->Array Int64 Int64-> IO ()
 print_results [a,b] m array= print $ solution_diff (a,b,m) array
 print_results (a:b:xs) m array= do
   print $ solution_diff (a,b,m) array
   print_results xs m array
 
-readInts :: IO [Integer]
+readInts :: IO [Int64]
 readInts = fmap (map read.words) getLine
 
 main :: IO ()
 main = do
   [n,m] <- readInts
   lista <- read_input n []
-  print_results lista m (create_array m (maximum(lista)))
+  arr<-create_array m (maximum lista)
+  print_results lista m arr
