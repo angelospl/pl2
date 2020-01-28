957bc442282c831b082771b148f501966b9f0877
angelospl <angelospl48@gmail.com>
2020-01-28 18:50:31 +0200

    Start readExpr

--- Exercise5/TypeInfer.hs
+++ Exercise5/TypeInfer.hs
@@ -49,5 +49,36 @@ readOne  =  do  s <- getLine
 
 count n m  =  sequence $ take n $ repeat m
 
+readExpr::Expr->(Type,[(Type,Type)])
+readExpr expr =
+  where helper (Evar x) cnt constr=
+          if (givetype x == - 1) then (Tvar cnt,cnt+1,constr)
+          else (Tvar (givetype x),cnt,constr)
+
+--afora ton prwto kanona tou curry
+--epistrefei ton elegxo typ == tvar a
+--alliws an einai synarthsh a->b elegxei anadromika ean
+--einai typ == a kai typ==b. ola ginontai sto perivallon G pou 8ewritika
+--mas dinetai
+typeinenvironment::Type->Type->Bool
+typeinevironment typ (Tvar a) = typ == (Tvar a)
+typeinenvironment typ (Tfun a b) =
+  (typeenvironment typ a || typeinenvironment typ b)
+
+--pairnei ousiastika typous typ1->typ2 se ena perivallon constrlst (lista)
+--gyrnaei ti lista me tous periorismous efarmozontas tin synarthsh helper
+--
+functype::Type->Type->[(Type,Type)]->[(Type,Type)]
+functype _ _ []=[]
+functype typ1 typ2 ((t1,t2):constrlst) = (helper typ1 typ2 t1,helper typ1 typ2 t2)):(functype typ1 typ2 constrlst)
+  where helper::Type->Type->Type->Type
+        helper a b (Tvar c) =
+          if a== (Tvar c) then b
+          else Tvar c
+        helper a b (Tfun c d)=
+          if a == (Tfun c d) then b
+          else Tfun (helper a b c) (helper a b d)
+
+
 main     =  do  n <- readLn
                 count n readOne
