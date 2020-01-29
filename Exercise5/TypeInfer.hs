dba52211a3e3bb39cf872a0c0e0ad921a6107c5a
angelospl <angelospl48@gmail.com>
2020-01-29 19:32:31 +0200

    Add unify not working properly

--- Exercise5/TypeInfer.hs
+++ Exercise5/TypeInfer.hs
@@ -46,39 +46,96 @@ instance Show Type where
 readOne  =  do  s <- getLine
                 let e = read s :: Expr
                 putStrLn ("Parsed: " ++ show e)
+                print $ readExpr e
+                print $ unify (snd (readExpr e))
 
 count n m  =  sequence $ take n $ repeat m
 
+--diavazei mia ekfrasi expr kai gyrnaei mia toupla me ton typo tis expr
+--ka8ws kai mia lista me tous periorismous (touples typwn)
 readExpr::Expr->(Type,[(Type,Type)])
-readExpr expr =
-  where helper (Evar x) cnt constr=
-          if (givetype x == - 1) then (Tvar cnt,cnt+1,constr)
-          else (Tvar (givetype x),cnt,constr)
+readExpr expr = (retyp,retconstr)
+  where helper::Expr->(String->Int)->Int->[(Type,Type)]->(Type,Int,[(Type,Type)])
+        --an i ekfrasi einai evar an emfanizetai prwti fora lamvanei neo typo
+        --kai au3anei to pli8os ton diaforetikwn typwn (cnt + 1)
+        --den pros8etei kapoion periorismo.
+        --ousiastika einai o prwtos kanonas a la curry
+        helper (Evar x) f cnt constr =
+          if (f x == -1 ) then (Tvar cnt,cnt+1,constr)
+          else (Tvar (f x),cnt,constr)
 
---afora ton prwto kanona tou curry
---epistrefei ton elegxo typ == tvar a
---alliws an einai synarthsh a->b elegxei anadromika ean
---einai typ == a kai typ==b. ola ginontai sto perivallon G pou 8ewritika
---mas dinetai
-typeinenvironment::Type->Type->Bool
-typeinevironment typ (Tvar a) = typ == (Tvar a)
-typeinenvironment typ (Tfun a b) =
-  (typeenvironment typ a || typeinenvironment typ b)
+        --an i ekfrasi einai synarthsh var->expr kalei tin helper anadromika
+        --gia na vrei ton typo tis ekfrasis ka8ws kai tous neous periorismous
+        --oi typoi au3anontai kata enan ka8ws prosti8etai o typos epistrofis tis sinartisis
+        --o typos tis 8a einai typos var -> typos expr. ousiastika einai o 2os kanonas a la curry
+        helper (Eabs var expr) f cnt constr =
+          let
+            nfun = \x -> if x == var then cnt else fun x
+            (typ,ncnt,nconstr) = helper expr nfun (cnt+1) constr
+          in
+            (Tfun (Tvar cnt) typ,ncnt,nconstr)
+        --an exoume mia ekfrasi pou einai expr1 expr2 kaloume tin helper stin expr1
+        --gia na paroume ton typo ths kai tous periorismous klp
+        --epeita tin 3anakaloume se auta pou gyrizei gia tin expr2
+        --exoume tin voi8itiki synarthsh extr pou tin kaloume gia ton typo epistrofis tis expr1
+        --an einai synarthsh t1->t2 tote o typos epistrofis einai t2 kai pros8etoume kai pros8etoume ton periorismo
+        --oti t2 = tp1 (tp1 o typos tis expr1)
+        --an i expr1 einai tis morfis Tvar x tote o typos epistrofis einai kainourios typos ntyp2
+        --kai pros8etoume tous periorismous oti to type x = ntyp1->ntyp2 kai oti ntyp1 = tp1
+        --tritos kanonas a la curry
+        helper (Eapp expr1 expr2) f cnt constr =
+          let
+            (tp1,cnt1,constr1) = helper expr1 f cnt constr
+            (tp2,cnt2,constr2) = helper expr2 f cnt1 constr1
 
---pairnei ousiastika typous typ1->typ2 se ena perivallon constrlst (lista)
---gyrnaei ti lista me tous periorismous efarmozontas tin synarthsh helper
---
-functype::Type->Type->[(Type,Type)]->[(Type,Type)]
-functype _ _ []=[]
-functype typ1 typ2 ((t1,t2):constrlst) = (helper typ1 typ2 t1,helper typ1 typ2 t2)):(functype typ1 typ2 constrlst)
-  where helper::Type->Type->Type->Type
-        helper a b (Tvar c) =
-          if a== (Tvar c) then b
-          else Tvar c
-        helper a b (Tfun c d)=
-          if a == (Tfun c d) then b
-          else Tfun (helper a b c) (helper a b d)
+            extr (Tfun t1 t2) = (t2,cnt2,((t1,tp2):constr2))
+            extr (Tvar x) =
+              let
+                ntyp1 = Tvar cnt2
+                ntyp2 = Tvar (cnt2+1)
+                nfuntyp = Tfun ntyp1 ntyp2
+              in
+                (ntyp2,cnt2+2,((Tvar x,nfuntyp):(ntyp1,tp1):constr2))
+          in
+            extr tp1
+        fun x = -1
+        (retyp,_,retconstr) = helper expr fun 0 []
 
+inside::Type->Type->Bool
+typ `inside` (Tvar a) = typ == (Tvar a)
+typ `inside` (Tfun a b) =
+  (typ `inside` a || typ `inside` b)
+
+unify::[(Type,Type)]->Bool
+unify [] = True
+unify constr@((t1,t2):ts)
+  | t1 == t2 = unify ts
+  | not (matchFun t1) && (matchFun t2) &&  not (t1 `inside` t2) = True && unify (map (replace t1 t2) constr)
+  | not (matchFun t2) && (matchFun t1) && not (t2 `inside` t1) = True && unify (map (replace t2 t1) constr)
+  | matchFun t1 && matchFun t2 =
+    let
+      (nt1,nt2) = retFun t1
+      (ft1,ft2) = retFun t2
+    in
+      True && unify ((nt1,ft1):(nt2,ft2):constr)
+  | otherwise = False
+
+matchFun::Type->Bool
+matchFun (Tvar _) = False
+matchFun (Tfun _ _) = True
+
+retFun::Type->(Type,Type)
+retFun (Tfun t1 t2) = (t1,t2)
+
+replace::Type->Type->(Type,Type)->(Type,Type)
+replace a repl (t1,t2)= (helprepl t1 a repl,helprepl t2 a repl)
+  where helprepl::Type->Type->Type->Type
+        helprepl (Tvar x) a repl =
+          if (Tvar x) == a then repl
+          else Tvar x
+        helprepl (Tfun x y) a repl =
+          if (Tfun x y) == a then repl
+          else Tfun (helprepl x a repl) (helprepl y a repl)
 
 main     =  do  n <- readLn
                 count n readOne
