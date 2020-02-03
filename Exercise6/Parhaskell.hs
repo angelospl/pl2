1d82b9e9d02b27f254e242acfb9d2b2804b42137
angelospl <angelospl48@gmail.com>
2020-02-03 20:10:32 +0200

    Parallel ok. Concurrent not working

--- Exercise6/Parhaskell.hs
+++ Exercise6/Parhaskell.hs
@@ -1,4 +1,5 @@
 import Control.Parallel.Strategies
+import Control.Concurrent
 
 perms::Integer->Integer->Integer
 perms n 0 = 1
@@ -28,8 +29,38 @@ transform_res (n:k:p:xs) = (n,k,p):transform_res xs
 par_calc_results::[(Integer,Integer,Integer)]->Eval [Integer]
 par_calc_results xs= mapM (rpar . simple_calc) xs
 
+-- conc_calc_result::MVar (Integer,(Integer,Integer,Integer))-> IO ()
+-- conc_calc_result mvar = do
+--   (_,x) <- readMVar mvar
+--   let res = simple_calc x
+--   putMVar mvar (res,x)
+
+-- loop::[MVar (Integer,(Integer,Integer,Integer))]->IO ()
+-- loop [] = return ()
+-- loop (mvar:mvars)= do
+--   forkIO (conc_calc_result mvar)
+--   loop mvars
+
+conc_calc_results::[(Integer,Integer,Integer)]->[MVar (Integer,Integer,Integer)]
+conc_calc_results lst = do
+  let mvars = [mvar | j <- lst, mvar <- do newMVar j]
+  --loop mvars
+  return mvars
+
+--
+-- print_mvars [] = return ()
+-- print_mvars (x:xs) = do
+--   ret <- readMVar x
+--   print ret
+--   print_mvars xs
+
 main:: IO ()
 main = do
   [t] <- readInts
   lista <- read_input t []
-  mapM_ print (runEval (par_calc_results (transform_res lista)))
+  --serial
+  mapM_ print (calc_results (transform_res lista))
+  --parallel
+  -- mapM_ print (runEval (par_calc_results (transform_res lista)))
+  --concurrent
+  --print_mvars (conc_calc_results (transform_res lista))
