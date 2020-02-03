cbb45b3e4c9766ae01bc5375abdde2646d4817af
angelospl <angelospl48@gmail.com>
2020-02-03 22:41:05 +0200

    Concurrent still not working

--- Exercise6/Parhaskell.hs
+++ Exercise6/Parhaskell.hs
@@ -29,38 +29,32 @@ transform_res (n:k:p:xs) = (n,k,p):transform_res xs
 par_calc_results::[(Integer,Integer,Integer)]->Eval [Integer]
 par_calc_results xs= mapM (rpar . simple_calc) xs
 
--- conc_calc_result::MVar (Integer,(Integer,Integer,Integer))-> IO ()
--- conc_calc_result mvar = do
---   (_,x) <- readMVar mvar
---   let res = simple_calc x
---   putMVar mvar (res,x)
+conc_calc_result::MVar [Integer]->(Integer,Integer,Integer)->IO ()
+conc_calc_result mvar x = do
+  lst <- readMVar mvar
+  let res = simple_calc x
+  putMVar mvar (lst++[res])
 
--- loop::[MVar (Integer,(Integer,Integer,Integer))]->IO ()
--- loop [] = return ()
--- loop (mvar:mvars)= do
---   forkIO (conc_calc_result mvar)
---   loop mvars
+loop::MVar [Integer]->[(Integer,Integer,Integer)]->IO ()
+loop _ [] = return ()
+loop mvar (x:xs) = do
+  forkOn 4 (conc_calc_result mvar x)
+  loop mvar xs
 
-conc_calc_results::[(Integer,Integer,Integer)]->[MVar (Integer,Integer,Integer)]
+conc_calc_results::[(Integer,Integer,Integer)]->IO ()
 conc_calc_results lst = do
-  let mvars = [mvar | j <- lst, mvar <- do newMVar j]
-  --loop mvars
-  return mvars
-
---
--- print_mvars [] = return ()
--- print_mvars (x:xs) = do
---   ret <- readMVar x
---   print ret
---   print_mvars xs
+  mvar <- newEmptyMVar
+  loop mvar lst
+  ret <- readMVar mvar
+  print $ ret
 
 main:: IO ()
 main = do
   [t] <- readInts
   lista <- read_input t []
   --serial
-  mapM_ print (calc_results (transform_res lista))
+  --mapM_ print (calc_results (transform_res lista))
   --parallel
   -- mapM_ print (runEval (par_calc_results (transform_res lista)))
   --concurrent
-  --print_mvars (conc_calc_results (transform_res lista))
+  print_mvars (conc_calc_results (transform_res lista))
