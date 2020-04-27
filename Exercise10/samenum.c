4bb421a545e8a994121c44ebf11913a28a401707
angelospl <angelospl48@gmail.com>
2020-04-27 22:27:53 +0300

    Change frama 30/33. Not found solution

--- Exercise10/samenum.c
+++ Exercise10/samenum.c
@@ -6,12 +6,12 @@
   @*/
 
 /*@ predicate noothersol{L}(int n,int res, int *a) =
-  @     \forall integer i,j; 0<= i < j < n ==> a[i] == a[j] ==> res >= j - i ;
+  @     \forall integer i,j; 0<= i < j < n && \at(a[i],L) == \at(a[j],L) ==> res >= j - i ;
   @
   @*/
 
 /*@ predicate validres{L}(int n, int res, int *a) =
-  @     res == 0 ? solexists {L} (n,res,a) : noothersol {L} (n,res,a);
+  @     noothersol {L} (n,res,a) ? solexists {L} (n,res,a) : res == 0;
   @*/
 
 
@@ -19,29 +19,33 @@
   @ requires \valid (x + (0..(N-1)));
   @ requires \forall integer i; 0 <= i < N ==> 1 <= x[i] < MAXV;
   @ assigns \nothing;
-  @ ensures 0 <= \result <= MAXV;
+  @ ensures 0 <= \result < N;
   @ ensures validres(N,\result,x);
 @*/
 int samenum(int N,int *x) {
   int p[MAXV+1];
   /*@ loop assigns i, p[0..(MAXV)];
-    @ loop invariant 0 <= i <= (MAXV + 1);
+    @ loop invariant 0 <= i <= (MAXV+1);
     @ loop invariant \forall integer k; 0 <= k < i ==> p[k] == -1;
     @ loop variant MAXV - i;
   @*/
   for (int i = 0;i <= MAXV; ++i) p[i] = -1;
-  /*@ assigns best; @*/
   int best = 0;
 
   /*@ loop assigns i , p[0..(MAXV)],best;
-    @ loop invariant 0 <= i < N;
-    @ loop invariant \forall integer k; 0<= k < i ==> p[x[k]] > -1;
-    @ loop invariant \forall integer k; 0<= k < i ==> best >= i - p[x[k]];
+    @ loop invariant 0 <= i <= N;
+    @ loop invariant \forall integer k; 0 <= k <= MAXV ==> -1 <= p[k] < N;
+    @ loop invariant \forall integer k; 0<= k < i ==>
+    @      \exists integer y; y < k && x[y] == x[k] ==> best == k - p[x[y]]; 
     @ loop variant N - i;
   @*/
   for (int i = 0; i < N; ++i)
-    if (p[x[i]] == -1) p[x[i]]= i;
-    else if (i-p[x[i]] > best) best = i - p[x[i]];
+    if (p[x[i]] == -1) {
+      p[x[i]]= i;
+    }
+    else {
+      if (i-p[x[i]] > best) best = i - p[x[i]];
+    }
   return best;
 
 }
